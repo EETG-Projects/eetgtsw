e4ecfc32a9a42deea31da4a35f455900484eed6c
tfpikachu <pascalneuber182@gmail.com>
2026-08-05 00:00:28 +0200

    .

---
+++ assets/data/fahrzeuge.js
@@ -0,0 +1,194 @@
+/*
+  EETG-Fahrzeugdaten
+  ==================
+  Hier kannst du Fahrzeuge ändern, entfernen oder ergänzen.
+
+  WICHTIG:
+  - Jeder Eintrag steht zwischen { und }.
+  - Zwischen zwei Fahrzeugen muss ein Komma stehen.
+  - Erlaubte Kategorien: "electric", "diesel", "trainset"
+  - Ein Bild ist optional. Lege es z. B. unter assets/img/fahrzeuge/ ab
+    und trage den Pfad bei "bild" ein. Ohne Bild erscheint die grafische Lok-Silhouette.
+*/
+window.EETG_VEHICLES = [
+  {
+    name: "BR 101",
+    kategorie: "electric",
+    typ: "Elektrolokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "200 km/h",
+    leistung: "6.400 kW",
+    bild: ""
+  },
+  {
+    name: "BR 103",
+    kategorie: "electric",
+    typ: "Elektrolokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "200 km/h",
+    leistung: "7.440 kW",
+    bild: ""
+  },
+  {
+    name: "BR 110",
+    kategorie: "electric",
+    typ: "Elektrolokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "140 km/h",
+    leistung: "3.260 kW",
+    bild: "assets/img/fahrzeuge/110488.png"
+  },
+  {
+    name: "BR 111",
+    kategorie: "electric",
+    typ: "Elektrolokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "160 km/h",
+    leistung: "3.700 kW",
+    bild: ""
+  },
+  {
+    name: "BR 1116",
+    kategorie: "electric",
+    typ: "Elektrolokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "230 km/h",
+    leistung: "6.400 kW",
+    bild: ""
+  },
+  {
+    name: "BR 112",
+    kategorie: "electric",
+    typ: "Elektrolokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "120 km/h",
+    leistung: "4.000 kW",
+    bild: ""
+  },
+  {
+    name: "BR 114",
+    kategorie: "electric",
+    typ: "Elektrolokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "160 km/h",
+    leistung: "4.000 kW",
+    bild: ""
+  },
+  {
+    name: "BR 140",
+    kategorie: "electric",
+    typ: "Elektrolokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "110 km/h",
+    leistung: "3.620 kW",
+    bild: "assets/img/fahrzeuge/140.png"
+  },
+  {
+    name: "BR 143",
+    kategorie: "electric",
+    typ: "Elektrolokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "120 km/h",
+    leistung: "3.500 kW",
+    bild: ""
+  },
+  {
+    name: "BR 145",
+    kategorie: "electric",
+    typ: "Elektrolokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "140 km/h",
+    leistung: "4.200 kW",
+    bild: ""
+  },
+  {
+    name: "BR 146.2",
+    kategorie: "electric",
+    typ: "Elektrolokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "160 km/h",
+    leistung: "5.600 kW",
+    bild: ""
+  },
+  {
+    name: "BR 155",
+    kategorie: "electric",
+    typ: "Elektrolokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "120 km/h",
+    leistung: "5.100 kW",
+    bild: ""
+  },
+  {
+    name: "BR 185.2",
+    kategorie: "electric",
+    typ: "Elektrolokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "140 km/h",
+    leistung: "5.600 kW",
+    bild: "assets/img/fahrzeuge/br1852.jpg"
+  },
+  {
+    name: "BR 204",
+    kategorie: "diesel",
+    typ: "Diesellokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "100 km/h",
+    leistung: "1.100 kW",
+    bild: "assets/img/fahrzeuge/204.png"
+  },
+  {
+    name: "BR 218",
+    kategorie: "diesel",
+    typ: "Diesellokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "140 km/h",
+    leistung: "2.060 kW",
+    bild: ""
+  },
+  {
+    name: "BR 294",
+    kategorie: "diesel",
+    typ: "Diesellokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "80 km/h",
+    leistung: "1.000 kW",
+    bild: ""
+  },
+  {
+    name: "BR 365",
+    kategorie: "diesel",
+    typ: "Diesellokomotive",
+    status: "EETG Flotte",
+    geschwindigkeit: "60 km/h",
+    leistung: "478 kW",
+    bild: ""
+  },
+  {
+    name: "BR 612",
+    kategorie: "trainset",
+    typ: "Dieseltriebzug",
+    status: "EETG Flotte",
+    geschwindigkeit: "160 km/h",
+    leistung: "1.120 kW",
+    bild: ""
+  },
+  {
+    name: "BR 628",
+    kategorie: "trainset",
+    typ: "Dieseltriebzug",
+    status: "EETG Flotte",
+    geschwindigkeit: "120 km/h",
+    leistung: "485 kW",
+    bild: ""
+  },
+  {
+    name: "BR 425",
+    kategorie: "trainset",
+    typ: "Elektrotriebzug",
+    status: "EETG Flotte",
+    geschwindigkeit: "140 km/h",
+    leistung: "2.350 kW",
+    bild: ""
+  }
+];
