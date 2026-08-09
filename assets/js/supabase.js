e85302544a053ff6b7a67ce386f0eb6f670d518b
EETG Pascal <pascalneuber182@gmail.com>
2026-08-09 16:22:00 +0200

    Update supabase.js

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,1 +1,17 @@
+const EETG_SUPABASE_URL =
+    "https://xirfavvxsoemyldqasba.supabase.co";
 
+const EETG_SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_88FG_I574HGxng5ZemRhQg_fAY3755f";
+
+window.eetgSupabase = supabase.createClient(
+    EETG_SUPABASE_URL,
+    EETG_SUPABASE_PUBLISHABLE_KEY,
+    {
+        auth: {
+            persistSession: true,
+            autoRefreshToken: true,
+            detectSessionInUrl: true
+        }
+    }
+);
